972d79c9c3ef203e4727e518037712d02e1aeabf
Conrad39 <conrad_vl@mail.ru>
2021-08-07 18:47:21 +0300

    Initial

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+        System.out.println(catBean == catBean2);
     }
 }
